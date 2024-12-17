299a9148826b0ff760b653a20455d4122b881e87
Thibaut Doreau <Atsu@Macbooks-MacBook-Pro.local>
2024-12-17 10:49:11 +0100

    Modification du Modele Produit pour sequelize

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,12 +1,21 @@
-const products = []; // Base de données fictive pour l'instant
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); 
 
-class Product {
-    constructor(id, name, price, quantity) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+const Product = sequelize.define('Product', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
-}
+}, {
+    timestamps: false, 
+});
 
-module.exports = { products, Product };
+module.exports = Product;
